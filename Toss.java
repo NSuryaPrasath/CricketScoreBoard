98634be51486eadf75f0e3bb0ced016eb193ab3f
N.Surya Prasath <84029114+NSuryaPrasath@users.noreply.github.com>
2022-10-03 13:00:13 +0530

    Update Toss.java

--- Toss.java
+++ Toss.java
@@ -6,7 +6,7 @@ public class Toss
 	public static String team1,team2;
 	public int userInput;
 	public static int autoValue;
-	public void toss(Map<String,String[]> hs,String[] team,int team1,int team2)
+	public void toss(String[] team,int team1,int team2)
 	{
 		TeamPlayer tp=new TeamPlayer();
 		Match mat=new Match();
@@ -16,8 +16,6 @@ public class Toss
 		autoValue=random.nextInt(1,3);
 		System.out.println("Enter 1 --> Head\n      2 --> Tail");
 		int user=sc.nextInt();
-//		team1=tp.team1;
-//		team2=tp.team2;
 		if(user==autoValue)
 		{
 			System.out.println(team[team1-1]+" won the toss");
@@ -26,19 +24,17 @@ public class Toss
 			if(playing==1)
 			{
 				System.out.println(team[team1-1]+" choose to Batting\n"+team[team2-1]+" has to Bowling");
-				mat.firstInnings(tp.hs,tp.team1player,tp.team2player,team,team1,team2);
+				mat.firstInnings(tp.team1player,tp.team2player,team,team1,team2);
 				System.out.println(team[team2-1]+" ==> BOWLING\n"+team[team1-1]+" ==> BATTING");
-				mat.secondInnings(tp.hs,tp.team2player,tp.team1player,team,team2,team1);
-				//mat.bowling(tp.team2player, team2);
+				mat.secondInnings(tp.team2player,tp.team1player,team,team2,team1);
 				mat.winings(team1,team2);
 			}
 			else
 			{
 				System.out.println(team[team1-1]+" choose to Bowling\n"+team[team2-1]+" has to Batting");
-				//mat.bowling(tp.team1player, team1);
-				mat.firstInnings(tp.hs,tp.team2player,tp.team1player,team, team2,team1);
+				mat.firstInnings(tp.team2player,tp.team1player,team, team2,team1);
 				System.out.println(team[team2-1]+" ==> Batting\n"+team[team1-1]+" ==> Bowling");
-				mat.secondInnings(tp.hs,tp.team1player,tp.team2player,team,team1,team2);
+				mat.secondInnings(tp.team1player,tp.team2player,team,team1,team2);
 				mat.winings(team1,team2);
 			}
 			mat.matchStart(tp.team1player,tp.team2player,team1,team2);
@@ -51,19 +47,17 @@ public class Toss
 			if(playing==1)
 			{
 				System.out.println(team[team2-1]+" choose to Batting\n"+team[team1-1]+" has to Bowling");
-				mat.firstInnings(tp.hs,tp.team2player,tp.team1player,team,team2,team1);
+				mat.firstInnings(tp.team2player,tp.team1player,team,team2,team1);
 				System.out.println(team[team1-1]+" ==> BATTING\n"+team[team2-1]+" ==> BOWLING");
-				mat.secondInnings(tp.hs,tp.team1player,tp.team2player,team,team1,team2);
-				//mat.bowling(tp.team1player, team1);
+				mat.secondInnings(tp.team1player,tp.team2player,team,team1,team2);
 				mat.winings(team1,team2);
 			}
 			else
 			{
 				System.out.println(team[team2-1]+" choose to Bowling\n"+team[team1-1]+" has to Batting");
-				//mat.bowling(tp.team2player, team2);
-				mat.firstInnings(tp.hs,tp.team1player,tp.team2player,team, team1,team2);
+				mat.firstInnings(tp.team1player,tp.team2player,team, team1,team2);
 				System.out.println(team[team1-1]+" ==> Batting\n"+team[team2-1]+" ==> Bowling");
-				mat.secondInnings(tp.hs,tp.team2player,tp.team1player,team,team2,team1);
+				mat.secondInnings(tp.team2player,tp.team1player,team,team2,team1);
 				mat.winings(team1,team2);
 			}
 		}
